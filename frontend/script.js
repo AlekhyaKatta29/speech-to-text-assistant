5621ec1ba93be2461e963df35bfc9892cf8f06b4
HariniKatta-13 <harinikatta13082004@gmail.com>
2026-07-02 20:50:29 +0530

    Updated frontend design

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,32 @@
 function startListening() {
 
-    document.getElementById("output").innerText =
-        "Listening started...";
+    const output = document.getElementById("output");
+
+    const recognition =
+        new webkitSpeechRecognition();
+
+    recognition.lang = "en-US";
+
+    recognition.onstart = function () {
+        output.innerText = "🎙️ Listening...";
+    };
+
+    recognition.onresult = function (event) {
+
+        let transcript =
+    event.results[0][0].transcript;
+
+transcript =
+    transcript.charAt(0).toUpperCase() +
+    transcript.slice(1);
+        output.innerText =
+            "✅ You said: " + transcript;
+    };
+
+    recognition.onerror = function () {
+        output.innerText =
+            "❌ Error occurred while listening.";
+    };
+
+    recognition.start();
 }
